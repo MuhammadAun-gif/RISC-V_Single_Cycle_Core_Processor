34c3170adc897c6606978e63e23cef16e3099313
Syed Muhammad Aun Hasan Naqavi <122987113+MuhammadAun-gif@users.noreply.github.com>
2024-12-02 15:39:13 +0500

    Create ALU_Mux.v

---
+++ ALU_Mux.v
@@ -0,0 +1,10 @@
+module ALU_Mux(SrcB,ALUSrc,RD2,Imm_Ext);
+       output [31:0] SrcB;
+       input ALUSrc;
+       input [31:0] RD2 ,Imm_Ext;
+
+
+       assign SrcB = (ALUSrc) ? Imm_Ext: RD2; 
+
+
+endmodule
